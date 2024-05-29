f31e4ec6173a47726af86bb07825b65ef91f4e4c
huangcpp <min@max.none>
2024-05-29 17:24:44 +0800

    hm extraArg

--- flake.nix
+++ flake.nix
@@ -41,7 +41,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.izumi = import ./home;
-
+            home-manager.extraSpecialArgs = inputs;
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
           }
